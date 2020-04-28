a29fcc5b024582c9023b0f77edf1cb0eeea14086
Martin Löffler <git@fatalerrorcoded.eu>
2020-04-29 00:35:20 +0200

    Find all partitions

--- stage_two/stage_two.asm
+++ stage_two/stage_two.asm
@@ -7,8 +7,37 @@ dd MENU_SIGNATURE
 align 16
 
 stage_two_start:
-    lea si, [msg.it_works]
-    call print
+    xor ax, ax
+    mov si, 0x6BE ; first partition entry
+    xor di, di
+    mov cx, 4
+get_next_partition_entry:
+    ; check if partition type is 0x0 (unused)
+    mov bl, byte [si + 0x4]
+    test bl, bl
+    jz .continue
+
+    ; partition number
+    mov byte [partitions + di], al
+    ; partition type
+    mov byte [partitions + 0x1 + di], bl
+    ; partition lba
+    mov ebx, dword [si + 0x8]
+    mov dword [partitions + 0x2 + di], ebx
+    ; partition status
+    mov bl, byte [si]
+    mov byte [partitions + 0x6 + di], bl
+    add di, 8
+
+.continue:
+    inc ax
+    dec cx
+    jz .end
+    add si, 0x10 ; next partition entry
+    jmp get_next_partition_entry
+
+.end:
+
     jmp $
 
 print:
@@ -28,5 +57,11 @@ print:
     pop eax
     ret
 
-msg:
-.it_works: db "It works!", 0
+; Layout:
+; 0x0 - Partition Number (used for writing the active bit to the right place on disk)
+; 0x1 - Partition Type
+; 0x2 - Partition LBA
+; 0x6 - Partition status
+; 0x7 - Unused
+partitions:
+    times (8 * 4) db 0
